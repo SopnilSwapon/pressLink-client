9b8f977009ad4ce0815a1843c789a196cd45c82f
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-06-02 09:03:59 +0600

    created basic files & setuped router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-600'>Vite + React</h1>
+      <p className="font-bold text-7xl">red</p>
+     
+    </>
+  )
+}
+
+export default App

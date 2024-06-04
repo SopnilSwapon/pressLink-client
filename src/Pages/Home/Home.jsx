121b09181fd3bc32178ca26852eb67686b2eebbd
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-06-04 18:02:09 +0600

    added userStatictics section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import TrendingNews from "./TrendingNews";
+import UsersStatictics from "./UsersStatictics";
 
 const Home = () => {
     return (
         <div className="min-h-[calc(100vh-313px)] text-red-500 pt-16">
             <TrendingNews></TrendingNews>
-            {/* <h2 className="text-5xl">This is home page</h2> */}
+            <UsersStatictics></UsersStatictics>
         </div>
     );
 };
